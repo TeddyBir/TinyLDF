f0dd7f4dc46627f20cc6049f133a3d04ace57b5c
Stéphane <stephane.ramahefarinaivo@etu.univ-nantes.fr>
2024-12-20 14:53:30 +0100

    fix(pom)

--- src/main/webapp/component/login.js
+++ src/main/webapp/component/login.js
@@ -5,7 +5,7 @@ var startApp = function() {
     gapi.load('auth2', function(){
         // Retrieve the singleton for the GoogleAuth library and set up the client.
         auth2 = gapi.auth2.init({
-            client_id: '     102077543806-hmf1pl4q83s3rlqr4r4pm6jf3cbt2jaj.apps.googleusercontent.com ',
+            client_id: '102077543806-hmf1pl4q83s3rlqr4r4pm6jf3cbt2jaj.apps.googleusercontent.com',
             cookiepolicy: 'single_host_origin',
             // Request scopes in addition to 'profile' and 'email'
             //scope: 'additional_scope'
